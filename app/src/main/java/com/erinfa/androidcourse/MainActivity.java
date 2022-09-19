c38a5dc7c02751f94dfeb401f7d7c00eee5a3256
Rakhi <rakhideveloperdixit@gmail.com>
2022-09-19 12:22:27 +0530

    main Activity done

--- app/src/main/java/com/erinfa/androidcourse/MainActivity.java
+++ app/src/main/java/com/erinfa/androidcourse/MainActivity.java
@@ -3,12 +3,26 @@ package com.erinfa.androidcourse;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+
+import com.erinfa.androidcourse.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+       // setContentView(R.layout.activity_main);
+        binding=ActivityMainBinding.inflate(getLayoutInflater());
+        View view=binding.getRoot();
+        setContentView(view);
+        binding.TextView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
